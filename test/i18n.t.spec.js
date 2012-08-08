6920f2e22535afe8f2001909c00f534bd472763f
Jan Muehlemann <jan.muehlemann@gmail.com>
2012-08-08 11:51:38 +0200

    array support

--- test/i18n.t.spec.js
+++ test/i18n.t.spec.js
@@ -22,6 +22,23 @@ describe('i18next.t.spec', function() {
 
   describe('translation functionality', function() {
 
+    describe('resource string as array', function() {
+      var resStore = {
+        dev: { translation: { testarray: ["title", "text"] } },
+        en: { translation: { } },            
+        'en-US': { translation: { } }
+      };
+      
+      beforeEach(function(done) {
+        i18n.init( i18n.functions.extend(opts, { resStore: resStore }),
+          function(t) { done(); });
+      });
+
+      it('it should translate nested value', function() {
+        expect(i18n.t('testarray')).to.be('title\ntext');
+      });
+    });
+
     describe('accessing nested values', function() {
 
       beforeEach(function(done) {
